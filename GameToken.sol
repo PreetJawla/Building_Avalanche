40a2d18dc8554b612ecfa93b09f074566abeeeca
PreetJawla <preetjawla6@gmail.com>
2024-07-06 01:11:38 +0530

    Update GameToken.sol

--- GameToken.sol
+++ GameToken.sol
@@ -7,49 +7,57 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
 
-contract DeganToken is ERC20("Degan Token", "DT"), ERC20Burnable, Ownable(msg.sender) {
-    string[4] public ItemForSale = ["1. Yellow Shirt","2. M416 Skin","3. Shotgun Skin","4. Legendery Marrine Outfint"]; 
+contract DeganToken is ERC20, ERC20Burnable, Ownable(msg.sender) {
+    constructor() ERC20( "Degen" ,"DGN"){}
+    function decimals() override public pure returns(uint8) {
+        return 0;
+    }
+    string[] public ItemForSale = ["1. Yellow Shirt","2. M416 Skin","3. Shotgun Skin","4. Legendery Marrine Outfit"]; 
     string[] public MyInvetory;
     
-    function mint1(address to, uint256 amount) public onlyOwner {
+    function mint1(address to, uint256 amount) external onlyOwner {
         _mint(to, amount*10**2);
     }
 
-    function burn(uint256 amount) public override {
+    function burntoken(uint256 amount) external {
         _burn(msg.sender, amount);
     }
 
-    function tranfer1(address to, uint256 amount) public {
-        //approve(msg.sender, amount);
+    function tranfer1(address to, uint256 amount) external  {
+        approve(msg.sender, amount);
         _transfer(msg.sender, to, amount);
     }
 
-    function getbalance() external view returns(uint256){
+    function getbalance() external  view returns(uint256){
         return this.balanceOf(msg.sender);
     }
 
-    function getItemsForSale() public view returns (string[4] memory) {
+    function getItemsForSale() external  view returns (string[] memory) {
         return ItemForSale;
     }
 
-        function getInventory() public view returns (string[] memory) {
+    function getInventory() external  view returns (string[] memory) {
         return MyInvetory;
     }
 
-    function StoreItem(uint _value) external returns(string memory){
+    function StoreItem(uint _value) external  returns(string memory){
         if (_value == 1) {
+            approve(msg.sender, _value);
             _burn(msg.sender,200);
             MyInvetory.push("Yellow Dress");
             return "You now have 1 yellow dress.";
         } else if (_value == 2) {
+            approve(msg.sender, _value);
             _burn(msg.sender,400);
-            MyInvetory.push("M416");
+            MyInvetory.push("M416 skin");
             return "You now have M416 skin";
         } else if (_value == 3) {
+            approve(msg.sender, _value);
             _burn(msg.sender,500);            
             MyInvetory.push("Shotgun Skin");
             return "You now have Shotgun skin";
         } else if(_value ==4) {
+            approve(msg.sender, _value);
             _burn(msg.sender,550);
             MyInvetory.push("Marrine Outfit");
             return "You now have a Legendery Marrine Outfit";
@@ -58,7 +66,6 @@ contract DeganToken is ERC20("Degan Token", "DT"), ERC20Burnable, Ownable(msg.se
             return "There is no item at such inedx for sale";
         }
     }
-
 
 
 }
